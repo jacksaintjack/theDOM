9e244ffa9b08da46f80ebbc754bda51d632993d3
Nicholas Glover <jacksaintjack@gmail.com>
2015-06-03 15:20:47 -0400

    finished first part of homework now going onto hard mode

--- main.js
+++ main.js
@@ -1,1 +1,35 @@
+$(document).ready(function() {
+  // anything i do in here is safe to interact with the document
+  // when i say 'safe', i mean the dom exists at this point
+  //
+  $('.addNumbers').click(function(event){
+  event.preventDefault();
+  var inputOne = Number($("#num_one").val());
+  var inputTwo = Number($("#num_two").val());
+   $("#answerAdd").html(inputOne + inputTwo)
 
+  });
+
+  $('.subtractNumbers').click(function(event){
+  event.preventDefault();
+  var inputOne = Number($("#num_one").val());
+  var inputTwo = Number($("#num_two").val());
+   $("#answer").html(inputOne - inputTwo)
+
+  });
+
+  $('.multiplyNumbers').click(function(event){
+  event.preventDefault();
+  var inputOne = Number($("#num_one").val());
+  var inputTwo = Number($("#num_two").val());
+   $("#answer").html(inputOne * inputTwo)
+
+  });
+
+  $('.divideNumbers').click(function(event){
+  event.preventDefault();
+  var inputOne = Number($("#num_one").val());
+  var inputTwo = Number($("#num_two").val());
+   $("#answer").html(inputOne / inputTwo)
+ });
+});
